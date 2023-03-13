25909ea3efef1b17a0d72e2a0ceb8beaad0a9cd7
czValeriy <chzhanfa@gmail.com>
2023-03-14 01:23:59 +0700

    Ex6

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,13 +1,30 @@
 import io.restassured.RestAssured;
+import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class HelloWorldTest {
     @Test
-    public void testHelloWorld(){
+    public void testRestAssured(){
+        Map<String, String> headers = new HashMap<>();
+        headers.put("MyHeader1", "MyHeader1");
+        headers.put("MyHeader2", "MyHeader2");
+
         Response response = RestAssured
-                .get ("https://playground.learnqa.ru/api/get_text")
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
                 .andReturn();
+
         response.prettyPrint();
+
+        String locationHeader = response.getHeader("Location");
+        System.out.println(locationHeader);
     }
 }
