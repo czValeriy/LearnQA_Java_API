b82bca2089c6c0fb790372ee132836d56472cf6e
czValeriy <chzhanfa@gmail.com>
2023-03-14 20:42:00 +0700

    Ex5

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -2,25 +2,15 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-
 
 public class HelloWorldTest {
     @Test
     public void testRestAssured(){
-        Map<String, String> params = new HashMap<>();
-        params.put("message", "And this is a second message");
-        params.put ("timestamp", "2021-06-04 16:41:51");
-
         JsonPath response = RestAssured
                 .given()
-                .queryParams(params)
                 .get("https://playground.learnqa.ru/api/get_json_homework")
                 .jsonPath();
 
-        String messages = response.get("message");
-        System.out.println(messages);
-
+        System.out.println ((String) response.get("messages[1].message"));
     }
 }
